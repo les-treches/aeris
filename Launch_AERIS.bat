36c5a83a4f1909fc734b3ab33e04d3382991bcd7
les_treches <sridhaarth.26014166@jainuniversity.ac.in>
2026-09-24 22:47:22 +0530

    Add Launch AERIS batch file

---
+++ Launch_AERIS.bat
@@ -0,0 +1,6 @@
+@echo off
+title AERIS Environmental Intelligence Platform
+echo Launching AERIS Environmental Intelligence Platform...
+cd /d "C:\Users\sridh\Desktop\hackathon\aeris"
+python serve.py
+pause
